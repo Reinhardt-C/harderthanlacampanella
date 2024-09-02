fcf69326d6d16b93c0c6003ffc5aec040ff6fc1b
Logan M <>
2024-09-02 21:03:14 +1200

    'aldskjfhskgd'

---
+++ pieces.js
@@ -0,0 +1,24 @@
+const PIECES = [
+    {
+        composer: "F. Liszt",
+        piece: "Mazeppa",
+        index: 4,
+        set: "Transcendental Etudes",
+        video: "lokglsP4HU8"
+    },
+    {
+        composer: "F. Liszt",
+        piece: "Feux follets",
+        index: 5,
+        set: "Transcendental Etudes",
+        video: "bhcBoNCZuBU"
+    },
+    {
+        composer: "F. Liszt",
+        piece: "Chasse-neige",
+        index: 12,
+        set: "Transcendental Etudes",
+        video: "5PqWXDo9TIw"
+    }
+
+]
